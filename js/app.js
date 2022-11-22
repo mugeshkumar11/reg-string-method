79dcd6f613af7707e8394a388a4a5c3762c19596
mugeshkumar <kmugesh11199@gmail.com>
2022-11-22 19:07:18 +0530

    add new file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 
 
-/// REGARDS METHOD USED STRING
+// REGARDS METHOD USED STRING
+
 var input = "kmugesh11199@gmail.com";
 console.log(input.match(/[a-zA-Z]+/g).join(""),input.match(/\W/g).join(""),input.match(/\d/g).join(""));
